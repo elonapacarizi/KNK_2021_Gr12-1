5389235fb21971fc1aa1be098b0089553425e327
Semire123 <semire.hyseni@student.uni-pr.edu>
2021-07-07 00:05:33 +0100

    Jane shtuar fajllat tek folderi Seats

--- Airline System/src/Seats/Plane_seat.java
+++ Airline System/src/Seats/Plane_seat.java
@@ -1,4 +1,24 @@
+
 package Seats;
 
-public class Plane_seat {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Plane_seat extends Application {
+    public Plane_seat() {
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = (Parent)FXMLLoader.load(this.getClass().getResource("/Seats/FXMLDocument.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
