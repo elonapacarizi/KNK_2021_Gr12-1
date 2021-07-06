492981661f38e5cfd572ffcea5e3bba2c9eb5541
unknown <rinese.kqiku@student.uni-pr.edu>
2021-07-06 23:46:30 +0200

    Added the code for Information file and two photos

--- Airline System/src/Info/Information.java
+++ Airline System/src/Info/Information.java
@@ -1,4 +1,36 @@
+
 package Info;
 
-public class Information {
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+
+public class Information extends Application {
+   
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Info/FXMLDocument.fxml"));
+         primaryStage.initStyle(StageStyle.UNDECORATED);
+         
+
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+         primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+        
+    }
+    
 }
