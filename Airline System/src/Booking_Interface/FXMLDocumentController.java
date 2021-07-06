fcc43315297c46857c6587016c862cbfc6342c09
LiresaFerizaj <74425676+LiresaFerizaj@users.noreply.github.com>
2021-07-06 22:40:16 +0200

    Update FXMLDocumentController.java

--- Airline System/src/Booking_Interface/FXMLDocumentController.java
+++ Airline System/src/Booking_Interface/FXMLDocumentController.java
@@ -1,4 +1,22 @@
 package Booking_Interface;
+import java.io.IOException;
 
-public class FXMLDocumentController {
-}
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class FXMLDocumentController implements Initializable {
+
+    
