5057f250c8ebb202ea3d586486cffef188cfa578
elonepacarizi <elonepacarizi@gmail.com>
2021-07-07 00:17:35 +0200

    trip updated

--- Airline System/src/destination/Trip.java
+++ Airline System/src/destination/Trip.java
@@ -1,4 +1,37 @@
+
 package destination;
 
-public class Trip {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+/**
+ *
+ * @author freec
+ */
+public class Trip extends Application {
+   
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/destination/FXMLDocument.fxml"));
+         primaryStage.initStyle(StageStyle.UNDECORATED);
+
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+         primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+        
+    }
+    
 }
