b4d52df38f47390c0d4c9cea970be5e9b6bafa55
sara.haliti@student.uni-pr.edu <sara.haliti@student.uni-pr.edu>
2021-07-06 22:51:44 +0200

    Main

--- Airline System/src/Main/FXMLDocumentController.java
+++ Airline System/src/Main/FXMLDocumentController.java
@@ -1,4 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Main;
 
-public class FXMLDocumentController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+
+public class FXMLDocumentController implements Initializable {
+    //-----------------
+  
+    
+    
+    
+    
+     static File file = new File("C:/Program Files/Airline System/src/Videos/Intro.mp4");
+
+static URI p= file.toURI(); 
+
+  static    Media m= new Media(p.toString());
+    static   MediaPlayer mp= new MediaPlayer(m);
+       
+      @FXML
+   public MediaView mv;
+
+   @FXML
+   private Button skip;
+  
+   
+    @FXML
+    private void Onskip(ActionEvent event) throws IOException {
+    
+
+         mp.stop();
+             Parent root = FXMLLoader.load(getClass().getResource("/Booking_Interface/FXMLDocument.fxml"));
+        Stage primaryStage= new Stage();
+    primaryStage.initStyle(StageStyle.UNDECORATED);
+      
+        
+         Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+         primaryStage.show();
+
+     Stage stage = (Stage) skip.getScene().getWindow();
+ 
+    stage.close();
+    
+      
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+      
+       
+      
+       mv.setFitHeight(720);
+       mv.setFitWidth(1280);
+        mv.setMediaPlayer(mp);
+       mp.play();
+       if(mp.getStatus()==MediaPlayer.Status.STOPPED){
+              
+
+   
+          new  Stage().close();
+ 
+       }
+    }    
+                               
+
+    
 }
--- Airline System/src/Main/Intro.java
+++ Airline System/src/Main/Intro.java
@@ -1,4 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Main;
 
-public class Intro {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author freec
+ */
+public class Intro extends Application {
+   
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Main/FXMLDocument.fxml"));
+         primaryStage.initStyle(StageStyle.UNDECORATED);
+      
+        
+         Scene scene = new Scene(root, 1280, 720);
+        primaryStage.setScene(scene);
+         primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+        
+    }
+    
 }
