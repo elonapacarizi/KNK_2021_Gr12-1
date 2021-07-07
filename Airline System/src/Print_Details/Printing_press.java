de21ffb604a17ec0f007e9f5fe480d18d3da1200
VeRoNa-KrAsNiQi <75127287+VeRoNa-KrAsNiQi@users.noreply.github.com>
2021-07-07 14:42:54 +0200

    U aktivizuan butonat te print ticket window

--- Airline System/src/Print_Details/Printing_press.java
+++ Airline System/src/Print_Details/Printing_press.java
@@ -1,4 +1,36 @@
+
 package Print_Details;
 
-public class Printing_press {
+import Payments.*;
+import Info.*;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class Printing_press extends Application {
+   
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Print_Details/FXMLDocument.fxml"));
+         primaryStage.initStyle(StageStyle.UNDECORATED);
+         
+         
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+         primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+        
+    }
+    
 }
