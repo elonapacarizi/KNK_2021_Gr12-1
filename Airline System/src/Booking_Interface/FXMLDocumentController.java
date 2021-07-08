1be3d23d543058401dae34a3d7654b7f015f01aa
LiresaFerizaj <74425676+LiresaFerizaj@users.noreply.github.com>
2021-07-08 22:57:15 +0200

    Update FXMLDocumentController.java

--- Airline System/src/Booking_Interface/FXMLDocumentController.java
+++ Airline System/src/Booking_Interface/FXMLDocumentController.java
@@ -1,4 +1,56 @@
 
+package Booking_Interface;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import Info.IFXMLDocumentController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author freec
+ */
+public class FXMLDocumentController implements Initializable {
+
+    
+    @FXML
+   private Button skip;
+    @FXML
+   private Button welcome;
+     @FXML
+   private Button login;
+      @FXML
+   private Button register;
+       @FXML
+   private Button help; 
+       @FXML
+   private Button about;
+   @FXML
+   private Button admin;
+ @FXML
+ private RadioButton ab,en;
+  
+  @FXML
+   private ImageView main;
+    @FXML
+    private Label l1,l2;
   
     
      @Override
