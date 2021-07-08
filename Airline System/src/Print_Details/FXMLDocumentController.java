5e7f10800ac88744e1c38e5f54159bbdc801d556
lbytyci <74509833+lbytyci@users.noreply.github.com>
2021-07-08 22:55:36 +0200

    Update FXMLDocumentController.java

--- Airline System/src/Print_Details/FXMLDocumentController.java
+++ Airline System/src/Print_Details/FXMLDocumentController.java
@@ -1,4 +1,3 @@
-
 package Print_Details;
 
 import Payments.*;
@@ -11,58 +10,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.Pane;
-
-public class FXMLDocumentController implements Initializable {
-    
-    
-      @FXML
-  private Pane rootpane;
-    
-@FXML
-private AnchorPane ap;
-   
-    @FXML
-   private Button back;
-   @FXML
-   private Button exit;
-  
-       @FXML
-    private void Onback(ActionEvent event) throws IOException {
-           Parent root = FXMLLoader.load(getClass().getResource("/Booking_Interface/FXMLDocument.fxml"));
-           Stage window= (Stage)back.getScene().getWindow();
-           window.setScene(new Scene(root));
-           window.centerOnScreen();
-    }
-    
-    @FXML
-    private void Onnext(ActionEvent event) throws IOException {
-
-        Stage stage = (Stage) exit.getScene().getWindow();
-
-        stage.close();
-      
-    }
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-      
-     ap.setBackground(Background.EMPTY);
-       }
-    
-                               
-
-    
-}
