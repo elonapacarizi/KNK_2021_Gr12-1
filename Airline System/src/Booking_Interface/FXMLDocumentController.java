79d34100461a30040a114586de651d17c20f3320
LiresaFerizaj <74425676+LiresaFerizaj@users.noreply.github.com>
2021-07-08 22:56:54 +0200

    Update FXMLDocumentController.java

--- Airline System/src/Booking_Interface/FXMLDocumentController.java
+++ Airline System/src/Booking_Interface/FXMLDocumentController.java
@@ -1,42 +1,4 @@
-package Booking_Interface;
-import java.io.IOException;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
-
-public class FXMLDocumentController implements Initializable {
- @FXML
-   private Button skip;
-    @FXML
-   private Button welcome;
-     @FXML
-   private Button login;
-      @FXML
-   private Button register;
-       @FXML
-   private Button help; 
-       @FXML
-   private Button about;
-   @FXML
-   private Button admin;
- 
-  
-  @FXML
-   private ImageView main;
-    
   
     
      @Override
