ac4975194cd44104e369e581e3a8ea825999c476
unknown <rinese.kqiku@student.uni-pr.edu>
2021-07-09 00:09:10 +0200

    Added the Ontext function in Info controller file

--- Airline System/src/Info/FXMLDocumentController.java
+++ Airline System/src/Info/FXMLDocumentController.java
@@ -122,11 +122,95 @@ private AnchorPane ap;
     @FXML
     private void Onnext(ActionEvent event) throws IOException {
 
+
+        FileOutputStream out = null;
+        File filet1 = new File("C:/Program Files/Airline System/src/Text_Files/name.txt");
+
+        String f1=filet1.getAbsolutePath();
+
+        String text = name.getText();
+        byte b[] = text.getBytes();
+
+        String outputFileName = System.getProperty("user.home",
+                File.separatorChar + "home"
+                        + File.separatorChar + "monica")
+                + File.separatorChar + "text.txt";
+        out = new FileOutputStream(f1);
+        out.write(b);
+        out.close();
+        FileOutputStream out1 = null;
+        File filet2 = new File("C:/Program Files/Airline System/src/Text_Files/passenger.txt");
+
+        String f2=filet2.getAbsolutePath();
+
+        String text2 =pass.getText() ;
+        byte b1[] = text2.getBytes();
+
+        String outputFileName1= System.getProperty("user.home",
+                File.separatorChar + "home"
+                        + File.separatorChar + "monica")
+                + File.separatorChar + "text.txt";
+        out1 = new FileOutputStream(f2);
+        out1.write(b1);
+        out1.close();
+        FileOutputStream out2 = null;
+        File filet3 = new File("C:/Program Files/Airline System/src/Text_Files/phone.txt");
+
+        String f3=filet3.getAbsolutePath();
+
+        String text3 =phone.getText() ;
+        byte b2[] = text3.getBytes();
+
+        String outputFileName2= System.getProperty("user.home",
+                File.separatorChar + "home"
+                        + File.separatorChar + "monica")
+                + File.separatorChar + "text.txt";
+        out2 = new FileOutputStream(f3);
+        out2.write(b2);
+        out2.close();
+
+
+
+        FileOutputStream out3= null;
+        File filet4 = new File("C:/Program Files/Airline System/src/Text_Files/time.txt");
+
+        String f4=filet4.getAbsolutePath();
+
+        String text4 =time.getText() ;
+        byte b3[] = text4.getBytes();
+
+        String outputFileName3= System.getProperty("user.home",
+                File.separatorChar + "home"
+                        + File.separatorChar + "monica")
+                + File.separatorChar + "text.txt";
+        out3 = new FileOutputStream(f4);
+        out3.write(b3);
+        out3.close();
+
+        FileOutputStream out4= null;
+        File filet5 = new File("C:/Program Files/Airline System/src/Text_Files/PlaneClass.txt");
+
+        String f5=filet5.getAbsolutePath();
+
+        String text5 =SClass
+                .getText() ;
+        byte b4[] = text5.getBytes();
+
+        String outputFileName4= System.getProperty("user.home",
+                File.separatorChar + "home"
+                        + File.separatorChar + "monica")
+                + File.separatorChar + "text.txt";
+        out4 = new FileOutputStream(f5);
+        out4.write(b4);
+        out4.close();
+
+
+
         Parent root = FXMLLoader.load(getClass().getResource("/Payments/FXMLDocument.fxml"));
         Stage window= (Stage)next.getScene().getWindow();
         window.setScene(new Scene(root));
         window.centerOnScreen();
-      
+
     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
