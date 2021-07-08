bdcd7882e68f98b84d200d7b7fe61bf3e3191796
SarandaQevani20 <>
2021-07-08 23:40:50 +0200

    Transaction file completed

--- Airline System/src/Payments/Transaction.java
+++ Airline System/src/Payments/Transaction.java
@@ -34,3 +34,42 @@ public class Transaction extends Application {
             }
         };
         scene.getAccelerators().put(kc1, rn1);
+
+        
+        KeyCombination kc2= new KeyCodeCombination(KeyCode.B, KeyCodeCombination.CONTROL_DOWN);
+        Runnable rn2 = ()-> {
+            try {
+                start2(primaryStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+        scene.getAccelerators().put(kc2, rn2);
+        KeyCombination kc= new KeyCodeCombination(KeyCode.Z, KeyCodeCombination.CONTROL_DOWN);
+        Runnable rn = ()-> System.exit(0);
+        scene.getAccelerators().put(kc, rn);
+        primaryStage.setScene(scene);
+         primaryStage.show();
+    }
+    public void start1(Stage primaryStage) throws Exception{
+        Parent root1 = FXMLLoader.load(getClass().getResource("/Seats/FXMLDocument.fxml"));
+        Scene scene = new Scene(root1);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public void start2(Stage primaryStage) throws Exception{
+        Parent root1 = FXMLLoader.load(getClass().getResource("/Info/FXMLDocument.fxml"));
+        Scene scene = new Scene(root1);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+        
+    }
+    
+}
