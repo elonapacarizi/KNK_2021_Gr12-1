fd396bb0a0e4339fcc0ccdb4e88cd49cced92bb8
Semire123 <semire.hyseni@student.uni-pr.edu>
2021-07-08 23:13:08 +0100

    Eshte shtuar nje pjese e kodit ne Seats

--- Airline System/src/Seats/FXMLDocumentController.java
+++ Airline System/src/Seats/FXMLDocumentController.java
@@ -1,6 +1,8 @@
 
 package Seats;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,18 +12,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class FXMLDocumentController implements Initializable {
-    @FXML
-    private Pane rootpane;
-    @FXML
-    private AnchorPane ap;
-    @FXML
-    private Button Print;
+
 
     public FXMLDocumentController() {
     }
@@ -32,6 +30,68 @@ public class FXMLDocumentController implements Initializable {
         Stage window = (Stage)this.Print.getScene().getWindow();
         window.setScene(new Scene(root));
         window.centerOnScreen();
+    }
+
+    @FXML
+    private void OnButtons1(ActionEvent event) throws IOException {
+        this.message("Your Ticket No. Is- AXCr3je809");
+        FileOutputStream out = null;
+        File filet = new File("C:/Program Files/Airline System/src/Text_Files/Ticket.txt");
+        String f = filet.getAbsolutePath();
+        String text = "AXCr3je809";
+        byte[] b = text.getBytes();
+        String outputFileName = System.getProperty("user.home", File.separatorChar + "home" + File.separatorChar + "monica") + File.separatorChar + "text.txt";
+        out = new FileOutputStream(f);
+        out.write(b);
+        out.close();
+    }
+
+    @FXML
+    private void OnButtons2(ActionEvent event) throws IOException {
+        this.message("Your Ticket No. Is- Damse809");
+        FileOutputStream out = null;
+        File filet = new File("C:/Program Files/Airline System/src/Text_Files/Ticket.txt");
+        String f = filet.getAbsolutePath();
+        String text = "Damse809";
+        byte[] b = text.getBytes();
+        String outputFileName = System.getProperty("user.home", File.separatorChar + "home" + File.separatorChar + "monica") + File.separatorChar + "text.txt";
+        out = new FileOutputStream(f);
+        out.write(b);
+        out.close();
+    }
+
+    @FXML
+    private void OnButtons3(ActionEvent event) throws IOException {
+        this.message("Your Ticket No. Is- DJeruse8037");
+        FileOutputStream out = null;
+        File filet = new File("C:/Program Files/Airline System/src/Text_Files/Ticket.txt");
+        String f = filet.getAbsolutePath();
+        String text = "DJeruse8037";
+        byte[] b = text.getBytes();
+        String outputFileName = System.getProperty("user.home", File.separatorChar + "home" + File.separatorChar + "monica") + File.separatorChar + "text.txt";
+        out = new FileOutputStream(f);
+        out.write(b);
+        out.close();
+    }
+
+    @FXML
+    private void OnButtons4(ActionEvent event) throws IOException {
+        this.message("Your Ticket No. Is- DAcrese88df09");
+        FileOutputStream out = null;
+        File filet = new File("C:/Program Files/Airline System/src/Text_Files/Ticket.txt");
+        String f = filet.getAbsolutePath();
+        String text = "DAcrese88df09";
+        byte[] b = text.getBytes();
+        String outputFileName = System.getProperty("user.home", File.separatorChar + "home" + File.separatorChar + "monica") + File.separatorChar + "text.txt";
+        out = new FileOutputStream(f);
+        out.write(b);
+        out.close();
+    }
+
+    private void message(String x) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setContentText(x);
+        alert.show();
     }
 
     public void initialize(URL url, ResourceBundle rb) {
--- Airline System/src/Seats/Plane_seat.java
+++ Airline System/src/Seats/Plane_seat.java
@@ -1,10 +1,13 @@
-
 package Seats;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyCombination.Modifier;
 import javafx.stage.Stage;
 
 public class Plane_seat extends Application {
@@ -14,6 +17,28 @@ public class Plane_seat extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = (Parent)FXMLLoader.load(this.getClass().getResource("/Seats/FXMLDocument.fxml"));
         Scene scene = new Scene(root);
+        KeyCombination kc1 = new KeyCodeCombination(KeyCode.N, new Modifier[]{KeyCodeCombination.CONTROL_DOWN});
+        Runnable rn1 = () -> {
+            try {
+                this.start1(primaryStage);
+            } catch (Exception var3) {
+                var3.printStackTrace();
+            }
+
+        };
+        scene.getAccelerators().put(kc1, rn1);
+        KeyCombination kc = new KeyCodeCombination(KeyCode.Z, new Modifier[]{KeyCodeCombination.CONTROL_DOWN});
+        Runnable rn = () -> {
+            System.exit(0);
+        };
+        scene.getAccelerators().put(kc, rn);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void start1(Stage primaryStage) throws Exception {
+        Parent root1 = (Parent)FXMLLoader.load(this.getClass().getResource("/Print_Details/FXMLDocument.fxml"));
+        Scene scene = new Scene(root1);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
