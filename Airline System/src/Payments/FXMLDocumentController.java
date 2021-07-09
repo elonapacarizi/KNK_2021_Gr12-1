92154372f9ee5fcf285f249d7759d07f474623ed
SarandaQevani20 <>
2021-07-09 09:56:39 +0200

    FXMLDocumentController file modified

--- Airline System/src/Payments/FXMLDocumentController.java
+++ Airline System/src/Payments/FXMLDocumentController.java
@@ -9,7 +9,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -17,18 +19,21 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+import java.util.Scanner;
+
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.Pane;
 
-/**
- *
- * @author freec
- */
+import javax.swing.*;
+
+
 public class FXMLDocumentController implements Initializable {
     //-----------------
   
@@ -49,7 +54,42 @@ private AnchorPane ap;
        
     
     //---------
-   
+    @FXML
+    private TextField date,cvv,name,pass, phone;
+
+    @FXML
+    private RadioButton Credit,Net,Google,Paypal,ab,en;
+    @FXML
+    private DatePicker Cdate;
+    @FXML
+    private Label cp,cpm;
+    @FXML
+   private Button back;
+   @FXML
+   private Button next;
+    @FXML
+    private Button exit;
+    @FXML
+    public void KeysN(KeyEvent event) throws IOException{
+        if (event.getCode() == KeyCode.ENTER) {
+
+            Parent root = FXMLLoader.load(getClass().getResource("/Seats/FXMLDocument.fxml"));
+            Stage window= (Stage)next.getScene().getWindow();
+            window.setScene(new Scene(root));
+            window.centerOnScreen();
+        }
+        else if(event.getCode() == KeyCode.ESCAPE){
+
+            Parent root = FXMLLoader.load(getClass().getResource("/Info/FXMLDocument.fxml"));
+            Stage window= (Stage)back.getScene().getWindow();
+            window.setScene(new Scene(root));
+            window.centerOnScreen();
+        }
+        else if(event.getCode() == KeyCode.CONTROL){
+
+            System.exit(0);
+        }
+    }
     
     @FXML
    private Button back;
