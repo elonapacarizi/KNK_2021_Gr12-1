cc63ae2fc0420d2aa8ac2cb270afa08d594d8026
sara.haliti@student.uni-pr.edu <sara.haliti@student.uni-pr.edu>
2021-07-09 23:31:59 +0200

    Seats

--- Airline System/src/Seats/FXMLDocumentController.java
+++ Airline System/src/Seats/FXMLDocumentController.java
@@ -1,11 +1,8 @@
 
 package Seats;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
+import Info.*;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,117 +11,139 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Pane;
+
 
 public class FXMLDocumentController implements Initializable {
-    @FXML
-    private Pane rootpane;
-    @FXML
-    private AnchorPane ap;
-    @FXML
-    private Button Print;
-    @FXML
-    private Button b1;
-    @FXML
-    private Button b2;
-    @FXML
-    private Button b3;
-    @FXML
-    private Button b4;
-    @FXML
-    private Button b5;
-    @FXML
-    private Button b6;
-    @FXML
-    private Button b7;
-    @FXML
-    private Button b8;
-    @FXML
-    private Button b9;
-    @FXML
-    private Button b10;
-    @FXML
-    private Button b11;
-    @FXML
-    private Button b12;
 
-
-    public FXMLDocumentController() {
-    }
-
+      @FXML
+  private Pane rootpane;
+   
+@FXML
+private AnchorPane ap;
+   
+    
+    @FXML
+   private Button Print,b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12;
+  
+      
+    
     @FXML
     private void Printing(ActionEvent event) throws IOException {
-        Parent root = (Parent)FXMLLoader.load(this.getClass().getResource("/Print_Details/FXMLDocument.fxml"));
-        Stage window = (Stage)this.Print.getScene().getWindow();
+
+        Parent root = FXMLLoader.load(getClass().getResource("/Print_Details/FXMLDocument.fxml"));
+        Stage window= (Stage)Print.getScene().getWindow();
         window.setScene(new Scene(root));
         window.centerOnScreen();
+    
+      
     }
-
     @FXML
     private void OnButtons1(ActionEvent event) throws IOException {
-        this.message("Your Ticket No. Is- AXCr3je809");
+
+        message("Your Ticket No. Is- AXCr3je809");
         FileOutputStream out = null;
         File filet = new File("C:/Program Files/Airline System/src/Text_Files/Ticket.txt");
-        String f = filet.getAbsolutePath();
+
+        String f=filet.getAbsolutePath();
+
         String text = "AXCr3je809";
-        byte[] b = text.getBytes();
-        String outputFileName = System.getProperty("user.home", File.separatorChar + "home" + File.separatorChar + "monica") + File.separatorChar + "text.txt";
+        byte b[] = text.getBytes();
+
+        String outputFileName = System.getProperty("user.home",
+                File.separatorChar + "home"
+                        + File.separatorChar + "monica")
+                + File.separatorChar + "text.txt";
         out = new FileOutputStream(f);
         out.write(b);
         out.close();
     }
-
     @FXML
     private void OnButtons2(ActionEvent event) throws IOException {
-        this.message("Your Ticket No. Is- Damse809");
+        message("Your Ticket No. Is- Damse809");
         FileOutputStream out = null;
         File filet = new File("C:/Program Files/Airline System/src/Text_Files/Ticket.txt");
-        String f = filet.getAbsolutePath();
+
+        String f=filet.getAbsolutePath();
+
         String text = "Damse809";
-        byte[] b = text.getBytes();
-        String outputFileName = System.getProperty("user.home", File.separatorChar + "home" + File.separatorChar + "monica") + File.separatorChar + "text.txt";
+        byte b[] = text.getBytes();
+
+        String outputFileName = System.getProperty("user.home",
+                File.separatorChar + "home"
+                        + File.separatorChar + "monica")
+                + File.separatorChar + "text.txt";
         out = new FileOutputStream(f);
         out.write(b);
         out.close();
     }
-
     @FXML
     private void OnButtons3(ActionEvent event) throws IOException {
-        this.message("Your Ticket No. Is- DJeruse8037");
+        message("Your Ticket No. Is- DJeruse8037");
         FileOutputStream out = null;
         File filet = new File("C:/Program Files/Airline System/src/Text_Files/Ticket.txt");
-        String f = filet.getAbsolutePath();
+
+        String f=filet.getAbsolutePath();
+
         String text = "DJeruse8037";
-        byte[] b = text.getBytes();
-        String outputFileName = System.getProperty("user.home", File.separatorChar + "home" + File.separatorChar + "monica") + File.separatorChar + "text.txt";
+        byte b[] = text.getBytes();
+
+        String outputFileName = System.getProperty("user.home",
+                File.separatorChar + "home"
+                        + File.separatorChar + "monica")
+                + File.separatorChar + "text.txt";
         out = new FileOutputStream(f);
         out.write(b);
         out.close();
     }
-
     @FXML
     private void OnButtons4(ActionEvent event) throws IOException {
-        this.message("Your Ticket No. Is- DAcrese88df09");
+        message("Your Ticket No. Is- DAcrese88df09");
         FileOutputStream out = null;
         File filet = new File("C:/Program Files/Airline System/src/Text_Files/Ticket.txt");
-        String f = filet.getAbsolutePath();
+
+        String f=filet.getAbsolutePath();
+
         String text = "DAcrese88df09";
-        byte[] b = text.getBytes();
-        String outputFileName = System.getProperty("user.home", File.separatorChar + "home" + File.separatorChar + "monica") + File.separatorChar + "text.txt";
+        byte b[] = text.getBytes();
+
+        String outputFileName = System.getProperty("user.home",
+                File.separatorChar + "home"
+                        + File.separatorChar + "monica")
+                + File.separatorChar + "text.txt";
         out = new FileOutputStream(f);
         out.write(b);
         out.close();
     }
-
-    private void message(String x) {
-        Alert alert = new Alert(AlertType.INFORMATION);
+    private void message(String x){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setContentText(x);
         alert.show();
+
+
     }
 
+    @Override
     public void initialize(URL url, ResourceBundle rb) {
-    }
+      
+
+       }
+    
+                               
+
+    
 }
