10c86ec7330d4238477dff18d270e688a1fd43ef
elonepacarizi <elonepacarizi@gmail.com>
2021-07-09 15:25:55 +0200

    Trip modified

--- Airline System/src/destination/Trip.java
+++ Airline System/src/destination/Trip.java
@@ -5,16 +5,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 
-/**
- *
- * @author freec
- */
-public class Trip extends Application {
-   
+public class Trip extends Application  {
+
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/destination/FXMLDocument.fxml"));
@@ -22,16 +22,39 @@ public class Trip extends Application {
 
 
         Scene scene = new Scene(root);
+//
+        KeyCombination kc= new KeyCodeCombination(KeyCode.Z, KeyCodeCombination.CONTROL_DOWN);
+        Runnable rn = ()-> System.exit(0);
+        scene.getAccelerators().put(kc, rn);
+
+        KeyCombination kc1= new KeyCodeCombination(KeyCode.N, KeyCodeCombination.CONTROL_DOWN);
+        Runnable rn1 = ()-> {
+            try {
+                start1(primaryStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+        scene.getAccelerators().put(kc1, rn1);
+
+
+
         primaryStage.setScene(scene);
          primaryStage.show();
     }
+    public void start1(Stage primaryStage) throws Exception{
+        Parent root1 = FXMLLoader.load(getClass().getResource("/Info/FXMLDocument.fxml"));
+        Scene scene = new Scene(root1);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-    /**
-     * @param args the command line arguments
-     */
+
+    
     public static void main(String[] args) {
         launch(args);
         
     }
     
 }
+
